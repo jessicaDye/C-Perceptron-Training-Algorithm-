0f2a99c2d1e0f0bf7be464aeaa278dee0ab72385
Jessica Dye <jess.dye67@gmail.com>
2025-12-16 09:26:15 -0500

    updated some minor errors; added constructor

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include <iomanip>
-#include <fstream>
-#include <string>
-#include <sstream>
+    /* Perceptron Algorithm Implementation
+    By Jessica Dye, Niagara University  */
+    
+    #include <iostream>
+    #include <vector>
+    #include <cstdlib>
+    #include <ctime>
+    #include <cmath>
+    #include <iomanip>
+    #include <fstream>
+    #include <string>
+    #include <sstream>
 
-using namespace std;
+    using namespace std;
+
+
+    class Perceptron {
+    private:
+        vector<double> weights;
+        double bias;
+        double learningRate;
+
+        public:
+        Perceptron() {
+            bias = 0.0;
+            learningRate = 0.1;
+        }
+    }
 
 int main() {
-    cout << "Perceptron source (comments removed)." << endl;
+
+    cout<<"Perceptron Algorithm Source Code" << endl;
+
     return 0;
 }
