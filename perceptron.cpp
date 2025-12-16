3bac35641d259e569795694c4c7e5010948feced
Jessica Dye <jess.dye67@gmail.com>
2025-12-15 21:12:09 -0500

    First commit; started and saved the Perceptron.
    Started and saved to commit to GitHub.

---
+++ perceptron.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    cout << "Perceptron source (comments removed)." << endl;
+    return 0;
+}
